f3c89461c41f747fa040358516fe168cdd7c695c
Gleb Kozyrev <gleb@gkoz.com>
2015-02-10 14:25:50 +0200

    Add aes_gcm feature gate

--- openssl-sys/src/lib.rs
+++ openssl-sys/src/lib.rs
@@ -449,6 +449,7 @@ extern "C" {
     pub fn EVP_aes_128_xts() -> *const EVP_CIPHER;
     #[cfg(feature = "aes_ctr")]
     pub fn EVP_aes_128_ctr() -> *const EVP_CIPHER;
+    #[cfg(feature = "aes_gcm")]
     pub fn EVP_aes_128_gcm() -> *const EVP_CIPHER;
     pub fn EVP_aes_256_cbc() -> *const EVP_CIPHER;
     pub fn EVP_aes_256_ecb() -> *const EVP_CIPHER;
@@ -456,6 +457,7 @@ extern "C" {
     pub fn EVP_aes_256_xts() -> *const EVP_CIPHER;
     #[cfg(feature = "aes_ctr")]
     pub fn EVP_aes_256_ctr() -> *const EVP_CIPHER;
+    #[cfg(feature = "aes_gcm")]
     pub fn EVP_aes_256_gcm() -> *const EVP_CIPHER;
     pub fn EVP_rc4() -> *const EVP_CIPHER;
 
